572ee51deb40a31e7df47678a90497ac4c4c2740
isychevskyi <29248722+isychevskyi@users.noreply.github.com>
2021-03-05 13:34:22 +0200

    Add files via upload

---
+++ _dev/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const $ = require('jquery');
+const App = require('./modules/app.js');
+const Viewport = require('./modules/viewport.js');
+const Controls = require('./modules/controls.js');
+const Objects = require('./modules/objects.js');
+const Save = require('./modules/save.js');
+
+$(function(){
+//create the app.
+let app = new App();
+let viewport = new Viewport();
+let controls = new Controls();
+let objects = new Objects();
+let save = new Save();
+});
